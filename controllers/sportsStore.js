42386a513283a94a46aebf8b4e1fb736512264ac
briptimus <brituor@gmail.com>
2015-06-22 10:18:54 -0700

    implemented comunication with a deployd service, added the place order functionality which now is persisted to database

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,6 +1,8 @@
 angular.module("sportsStore")
-    .constant("dataUrl", "services/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    .constant("dataUrl", "http://localhost:5500/products")
+    .constant("orderUrl", "http://localhost:5500/orders")    
+    .controller("sportsStoreCtrl", function ($scope, $http, $location, 
+        dataUrl, orderUrl, cart) {
      
         $scope.data = {};
 
@@ -11,4 +13,19 @@ angular.module("sportsStore")
             .error(function (error) { console.log("promise error");
                 $scope.data.error = error || { status : 404 };                 
             });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                }).finally(function () {
+                    $location.path("/complete");
+                });
+        }            
     });
